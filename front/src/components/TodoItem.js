733097c477c1bc8019cec423538fc19c53a6bfb8
eipole <eipole@tomat.tomat.com>
2021-03-02 20:54:39 +0100

    toggling

--- front/src/components/TodoItem.js
+++ front/src/components/TodoItem.js
@@ -6,12 +6,19 @@ async function handleDelete(id, array, func) {
   const removed = array.filter((elem) => elem._id !== id)
   func(removed)
 }
-
 async function handleToggle(id, completed, allTodos, setAllTodos) {
+  const response = await toggleTodo(id, completed)
+  const newtodos = allTodos.map((elem) =>
+    elem._id === response._id ? { ...elem, completed: !elem.completed } : elem
+  )
+  setAllTodos(newtodos)
+}
+
+/* async function handleToggle(id, completed, allTodos, setAllTodos) {
   const response = await toggleTodo(id, completed)
   const newtodos = allTodos.filter((elem, i) => elem._id !== response._id)
   setAllTodos([...newtodos, response])
-}
+} */
 
 const TodoItem = ({ name, completed, id, date, allTodos, setAllTodos }) => (
   <li>
